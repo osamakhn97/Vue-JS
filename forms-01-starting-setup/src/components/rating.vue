5c6d245c76fa976475fd2c3d5c2b196fb0b1d3d1
osama <osamakhn97@gmail.com>
2021-12-22 12:41:35 +0500

    Working with forms

---
+++ forms-01-starting-setup/src/components/rating.vue
@@ -0,0 +1,51 @@
+<template>
+    <ul>
+        <li>
+            <button :class="{active:modelValue === 'poor'}"   type="button" @click="activate('poor')">Poor</button>
+        </li>
+        <li>
+            <button :class="{active:modelValue === 'average'}"  type="button" @click="activate('average')">Average</button>
+        </li>
+        <li>
+            <button :class="{active:modelValue === 'great'}"  type="button" @click="activate('great')">Great</button>
+        </li>
+    </ul>
+</template>
+<script>
+export default{
+    props:['modelValue'],
+    emits:['update:modelValue'],
+    // data(){
+    //     return {
+    //         isActive:this.modelValue
+            
+    //     }
+    // },
+    methods:{
+        activate(item){
+        //   this.isActive = item
+          this.$emit('update:modelValue',item)
+         
+          
+          
+
+        }
+    }
+}
+
+
+</script>
+
+<style  scoped>
+ul {
+    list-style : none;
+    display:flex;
+    
+}
+.active {
+    border:2px solid seagreen
+}
+button{
+    background: transparent;
+}
+</style>
